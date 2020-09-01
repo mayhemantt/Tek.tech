aec4a936f458ace11bad8c1e946a621964b53371
8bithemant <hd4207577@gmail.com>
2020-09-01 20:08:27 +0530

    Added the Directions to setup

--- server/server.js
+++ server/server.js
@@ -3,8 +3,75 @@
   
   
   To visualize below comments, add Better Comments VS Code Extension, or follow the Syntax
+  * After a successful installation edit the setting.json for the extension and add the following code.
   
   
+  {
+    "javascript.updateImportsOnFileMove.enabled": "always",
+    "powermode.enabled": true,
+    "editor.fontSize": 16,
+    "liveServer.settings.donotVerifyTags": true,
+    "better-comments.highlightPlainText": true,
+    "better-comments.tags": [
+
+        {
+            "tag": "!",
+            "color": "#FF2D00",
+            "strikethrough": false,
+            "underline": false,
+            "backgroundColor": "transparent",
+            "bold": true,
+            "italic": true
+        },
+        {
+            "tag": "?",
+            "color": "#3498DB",
+            "strikethrough": false,
+            "underline": false,
+            "backgroundColor": "transparent",
+            "bold": false,
+            "italic": false
+        },
+        {
+            "tag": "//",
+            "color": "#474747",
+            "strikethrough": true,
+            "underline": false,
+            "backgroundColor": "transparent",
+            "bold": false,
+            "italic": false
+        },
+        {
+            "tag": "to",
+            "color": "#FF8C00",
+            "strikethrough": false,
+            "underline": false,
+            "backgroundColor": "transparent",
+            "bold": false,
+            "italic": false
+        },
+        {
+            "tag": "*",
+            "color": "pink",
+            "strikethrough": false,
+            "underline": false,
+            "backgroundColor": "transparent",
+            "bold": false,
+            "italic": false
+        },
+        {
+            "tag": "",
+            "color":"blond",
+            "strikethrough":false,
+            "underline":false,
+            "backgroundColor": "transparent",
+            "bold": false,
+            "italic": true
+        }
+    ]
+}
+
+  // ---------------------------------------------------------------------------------//
  Project Mains
  * ?Help - Question - Suggestion
  * TODO: Todo
@@ -12,28 +79,12 @@
  * * Important (40%)
  * !Warning (Much Important)
  * 
- 
  //// before it was this- ____ now i added this-____ 
  * TODO: Why added?
 
+
+   // ---------------------------------------------------------------------------------//
  */
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const express= require('express')
