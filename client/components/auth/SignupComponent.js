44aa72d52ee23fd740121599a69838f4a0676b7d
8bithemant <hd4207577@gmail.com>
2020-09-02 22:50:00 +0530

    Initial Submit

--- client/components/auth/SignupComponent.js
+++ client/components/auth/SignupComponent.js
@@ -1,6 +1,52 @@
+import {useState} from 'react'
+
+
 const SignupComponent=()=>{
+
+    const [values, setValues]=useState({
+        name:'',
+        email:'',
+        password:'',
+        error:'',
+        loading:false,
+        message:'',
+        showForm:true
+    })
+
+    const {name,email,password,error,loading,message,showForm}=values
+
+    const handleSubmit=(e)=>{
+        e.preventDefault()
+        console.table({name,email,password,error,loading,message,showForm})
+    }
+
+    const handleChange=(name)=>e=>{
+        setValues({...values, error:false,[name]:e.target.value})
+    }
+
+    const signupForm=()=>{
+        return (
+            <form onSubmit={handleSubmit}>
+                <div className="form-group">
+                    <input value={name} onChange={handleChange('name')} type="text" className="form-control" placeholder="Type Your Name"/>
+                </div>  
+                <div className="form-group">
+                    <input value={email} onChange={handleChange('email')} type="email" className="form-control" placeholder="Type Your Email"/>
+                </div> 
+                <div className="form-group">
+                    <input value={password} onChange={handleChange('password')} type="text" className="form-control" placeholder="Type Your Password"/>
+                </div> 
+                <div>
+                    <button className="btn btn-primary">SignUp</button>
+                </div>
+            </form>
+        )
+    }
+
     return(
-        <h2>Signup</h2>
+        <React.Fragment>
+            {signupForm()}
+        </React.Fragment>
     )
 }
 
