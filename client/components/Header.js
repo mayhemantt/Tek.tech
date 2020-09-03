34f96c7e5c74b30ff9afc37451c7a3cb8c4dc4bd
8bithemant <hd4207577@gmail.com>
2020-09-03 20:33:14 +0530

    Category Model

--- client/components/Header.js
+++ client/components/Header.js
@@ -47,6 +47,29 @@ const Header = () => {
               </React.Fragment>
             }
             {
+            isAuth() && isAuth().role===0 &&(
+              <NavItem>
+                <Link href="/user">  
+              <NavLink style={{cursor:'pointer'}} >
+                {`${isAuth().name}`}
+              </NavLink>
+              </Link>
+              </NavItem>
+            )
+            }
+
+            {
+            isAuth() && isAuth().role===1 && (
+              <NavItem>
+                <Link href="/admin">  
+              <NavLink style={{cursor:'pointer'}} >
+                {`${isAuth().name}`}
+              </NavLink>
+              </Link>
+              </NavItem>
+            )
+            }
+            {
             isAuth() && (
               <NavItem>
               <NavLink style={{cursor:'pointer'}} onClick={()=>signout(()=> Router.replace(`/signin`))}>Signout</NavLink>
