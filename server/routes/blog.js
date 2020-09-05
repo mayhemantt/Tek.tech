ebc7ce0ba90766d3085936ec4ebeb0f02cce2a53
8bithemant <hd4207577@gmail.com>
2020-09-05 21:19:14 +0530

    Blog Up's

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { create } = require('../controllers/blog');
+const { create, list, listAllBlogsCategoriesTags, read, remove, update} = require('../controllers/blog');
 
 const { requireSignin, adminMiddleware } = require('../controllers/auth');
 
@@ -8,6 +8,7 @@ router.post('/blog', requireSignin, adminMiddleware, create);
 router.get('/blogs', list);
 router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
 router.get('/blog/:slug', read);
-
+router.delete('/blog/:slug',requireSignin, adminMiddleware, remove);
+router.put('/blog/:slug',requireSignin, adminMiddleware, update);
 
 module.exports = router;
