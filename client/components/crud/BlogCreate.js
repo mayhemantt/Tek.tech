b93fcc4d8a32de33dcafc0f04d67757d027fa69a
8bithemant <hd4207577@gmail.com>
2020-09-05 11:15:25 +0530

    Blog Up's

--- client/components/crud/BlogCreate.js
+++ client/components/crud/BlogCreate.js
@@ -1,7 +1,19 @@
+import Link from 'next/link'
+import {useEffect, useState} from 'react'
+import Router from 'next/router'
+import dynamic from 'next/dynamic'
+import {withRouter} from 'next/router'
+import {getCookie, isAuth} from '../../actions/auth'
+import {getCategories } from '../../actions/category'
+import {getTags } from '../../actions/tags'
+import {createBlog} from '../../actions/blog'
+import '../../node_modules/react-quill/dist/quill.snow.css'
+const ReactQuill = dynamic(()=> import('react-quill'), {ssr: false})
+
 const CreateBlog=()=>{
-    return <div>
-        <h2> Create Blog Form</h2>
-    </div>
+    return  <div>
+                <h2> Create Blog Form</h2>
+            </div>
 }
 
 export default CreateBlog
