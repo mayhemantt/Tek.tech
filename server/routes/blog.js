536e5c651204c13b54df3a2adef8487969f15f16
8bithemant <hd4207577@gmail.com>
2020-09-05 20:54:29 +0530

    Blog Up's

--- server/routes/blog.js
+++ server/routes/blog.js
@@ -6,6 +6,8 @@ const { requireSignin, adminMiddleware } = require('../controllers/auth');
 
 router.post('/blog', requireSignin, adminMiddleware, create);
 router.get('/blogs', list);
-router.get('/blogs', list);
+router.post('/blogs-categories-tags', listAllBlogsCategoriesTags);
+router.get('/blog/:slug', read);
+
 
 module.exports = router;
