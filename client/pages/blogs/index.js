f1524feb2c15651321bb129c0a85cb1bebe1fac6
8bithemant <hd4207577@gmail.com>
2020-09-07 23:09:34 +0530

    :tada:

--- client/pages/blogs/index.js
+++ client/pages/blogs/index.js
@@ -4,8 +4,49 @@ import Layout from '../../components/Layout'
 import {useState} from 'react'
 import {listBlogsWithCategoriesAndTags} from '../../actions/blog'
 import {API} from '../../config'
+import renderHTML from 'react-render-html'
+import moment from 'moment'
 
-const Blogs=()=>{
+
+const Blogs=({blogs, categories, tags, size})=>{
+
+    const showAllBlogs= ()=>{
+        return blogs.map((blog, i)=>{
+            return <article key={i}>
+                <div className="lead pb-4">
+                    <header>
+                        <Link href={`/blogs/${blog.slug}`}>
+                            <a>
+                                <h2 className="pt-3 pb-3 font-weight-bold">{blog.title}</h2>
+                            </a>
+                        </Link>
+                    </header>
+                    <section>
+                        <p className="mark ml-1 pt-2 pb-2">
+                            Written By {blog.postedBy.name} | Published {moment(blog.updatedAt).fromNow()}
+                        </p>
+                    </section>
+                    <section>
+                        <p>Blog Categories and Tags</p>
+                    </section>
+                    <div className="row">
+                        <div className="col-md-4">Image</div>
+                        <div className="col-md-8">
+                            <section>
+                                <div className="pb-3">{renderHTML(blog.excerpt)}</div>
+                                <Link href={`/blogs/${blog.slug}`}>
+                                    <a className="btn btn-primary pt-2">
+                                        Read More
+                                    </a>
+                                </Link>
+                            </section>
+                        </div>
+                    </div>
+                </div>
+                <hr />
+            </article>
+        })
+    }
     return (
             <Layout>
                 <main>
@@ -24,7 +65,7 @@ const Blogs=()=>{
                     <div className="container-fluid">
                         <div className="row">
                             <div className="col-md-12">
-                                Show All Blogs
+                                {showAllBlogs()}
                             </div>
                         </div>
                     </div>
@@ -49,3 +90,6 @@ Blogs.getInitialProps=()=>{
 }
 
 export default Blogs
+
+
+
