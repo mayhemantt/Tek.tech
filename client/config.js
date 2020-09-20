60e27c47a8332c3a2bd1d8b2814ccd19ce2c87bb
Hemant Joshi <hd4207577@gmail.com>
2020-09-20 06:59:05 -0700

    Test :bug:

--- client/config.js
+++ client/config.js
@@ -2,6 +2,8 @@ import getConfig from 'next/config'
 
 const {publicRuntimeConfig}=getConfig()
 
+
+
 export const API= publicRuntimeConfig.PRODUCTION ? 'https://blog.com' :'http://localhost:8000/api'
 
 export const APP_NAME= publicRuntimeConfig.APP_NAME? publicRuntimeConfig.APP_NAME : 'BLOG'
